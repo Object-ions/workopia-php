da50ff292effd30c9622bb9ee34b05e5ecd95875
object-ions <moshikoatia@gmail.com>
2024-01-19 14:31:11 -0800

    separate router to a new file

--- public/index.php
+++ public/index.php
@@ -3,8 +3,4 @@ require '../helpers.php';
 
 $uri = $_SERVER['REQUEST_URI'];
 
-if (array_key_exists($uri, $routes)) {
-  require(basePath($routes[$uri]));
-} else {
-  require(basePath($routes['404']));
-}
+require basePath('router.php');
