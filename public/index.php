ee07e885da7d1d41508cbd203fe2e0a9893c69b9
object-ions <moshikoatia@gmail.com>
2024-01-19 12:11:32 -0800

    add ht-access file to customize and control how the server handles requests (URL) and routes. Add basic routes

--- public/index.php
+++ public/index.php
@@ -1,4 +1,13 @@
 <?php
 require '../helpers.php';
 
-loadView('home');
+$routes = [
+  '/' => 'controllers/home.php',
+  '/listings' => 'controllers/listings/index.php',
+  '/listing/create' => 'controllers/listings/create.php',
+  '404' => 'controllers/error/404.php'
+];
+
+$uri = $_SERVER['REQUEST_URI'];
+
+inspectAndDie($uri);
