d548cf0b7b22d0c0477c7449d6d3d00e37e8321e
object-ions <moshikoatia@gmail.com>
2024-01-19 15:41:12 -0800

    add a router class that handle a different http (method) request

--- router.php
+++ router.php
@@ -1,10 +1,61 @@
 <?php
 
-$routes = require basePath('routes.php');
+class Router {
+  protected $routes = [];
 
-if (array_key_exists($uri, $routes)) {
-  require(basePath($routes[$uri]));
-} else {
-  http_response_code(404);
-  require(basePath($routes['404']));
+  public function registerRoute($method, $uri, $controller) {
+    $this -> routes = [
+      'method' => $method,
+      'uri' => $uri,
+      'controller' => $controller
+    ];
+  }
+
+  /**
+   * sdd a GET rout
+   * 
+   * @param string $uri
+   * @param string $controller
+   * @return void
+   */
+
+   public function get($uri, $controller) {
+    $this -> registerRoute('GET', $uri, $controller);
+   }
+
+  /**
+   * sdd a POST rout
+   * 
+   * @param string $uri
+   * @param string $controller
+   * @return void
+   */
+
+   public function post($uri, $controller) {
+    $this -> registerRoute('POST', $uri, $controller);
+   }
+   
+  /**
+   * sdd a PUT rout
+   * 
+   * @param string $uri
+   * @param string $controller
+   * @return void
+   */
+
+   public function put($uri, $controller) {
+    $this -> registerRoute('PUT', $uri, $controller);
+   }
+
+  /**
+   * sdd a Delete rout
+   * 
+   * @param string $uri
+   * @param string $controller
+   * @return void
+   */
+
+   public function delete($uri, $controller) {
+    $this -> registerRoute('DELETE', $uri, $controller);
+   }
 }
