76e7b39a0b226c92af1578843b65b506ac3f7117
object-ions <moshikoatia@gmail.com>
2024-01-19 10:27:40 -0800

    add a new helper to load view

--- helpers.php
+++ helpers.php
@@ -7,6 +7,24 @@
  * @return string
  */
 
+// from ->  ../views/partials/head.php
+// to -> views/partials/head.php
+
 function basePath($path = '') {
   return __DIR__ . '/' . $path;
+}
+
+/**
+ * Load a view
+ * 
+ * @param string $name
+ * @ return void
+ * 
+ */
+
+// from -> views/partials/head.php
+// to -> 'home'
+// so anytime we want to load a view we going to call that fx
+function loadView($viewName) {
+  require basePath("views/{$viewName}.view.php");
 }
